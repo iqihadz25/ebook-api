eeab8d64d281b37844072af920ef5c2cfeafd9e9
iqihadz25 <91859177+iqihadz25@users.noreply.github.com>
2022-11-02 00:01:32 +0700

    challenge-author

---
+++ ebook/app/Http/Controllers/AuthController.php
@@ -0,0 +1,11 @@
+{
+    public function me()
+    {
+    return [
+            'NIS' => 3103120197,
+            'Name' => "Rifqi Hadaya",
+            'phone' => "082226464660",
+            'class' => "XII RPL 6"
+        ];
+    }
+}
--- ebook/routes/api.php
+++ ebook/routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthorController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +21,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/me',[AuthController::class, 'me']);
+Route::resource('books', BookController::class)->except(
+    ['create', 'edit']
+);
+Route::resource('authors', AuthorController::class)->except(
+    ['create', 'edit']
+);
+
+
